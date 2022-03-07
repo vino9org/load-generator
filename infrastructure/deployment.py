2d901c8ef81a0c9f01d1d249d961c000ea03a4a4
Li Lin <guru.lin@gmail.com>
2022-03-07 23:39:56 +0800

    removed unused files

--- infrastructure/deployment.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import aws_cdk as cdk
-from load_generator import load-generator
-
-stack_name = os.environ.get("TESTING_STACK_NAME", "load-generator")
-app = cdk.App()
-load-generator(app, stack_name).build()
-app.synth()
