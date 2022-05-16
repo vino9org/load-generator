48269fae3db6c3a5d208a6a7dee16eb5c9c450fb
Li Lin <guru.lin@gmail.com>
2022-05-16 17:53:53 +0800

    refactor and add utility to fire single request

--- locustfile.py
+++ locustfile.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from gevent import monkey
 
 # must do this first in order to prevent ssl related errors down the road
@@ -7,68 +9,11 @@ from gevent import monkey
 monkey.patch_all()
 
 import os
-import random
-from datetime import datetime
 from typing import Any, Tuple
 
-import boto3
 from locust import HttpUser, between, run_single_user, tag, task  # fmt: off noqa
-from requests_aws4auth import AWS4Auth
 
-from seed_limits_table import read_seed_data
-
-_all_ids_: list[Tuple[str, str]] = []
-
-
-def all_ids() -> list[Tuple[str, str]]:
-    global _all_ids_
-    if not _all_ids_:
-        for items in read_seed_data():
-            for item in items:
-                _all_ids_.append((item["acc_id"], item["cust_id"]))
-        print(f"loaded {len(_all_ids_)} account numbers from seed data")
-    return _all_ids_
-
-
-def rand_account() -> Tuple[str, str]:
-    return random.choice(all_ids())
-
-
-def rand_fund_transfer_request() -> dict[str, Any]:
-    rand_amount = round(random.uniform(1.00, 100.10), 2)
-    debit_acc_id, debit_cust_id = rand_account()
-    credit_acc_id, _ = rand_account()
-    if debit_acc_id == credit_acc_id:
-        credit_acc_id, _ = rand_account()
-    return {
-        "debit_customer_id": debit_cust_id,
-        "debit_account_id": debit_acc_id,
-        "credit_account_id": credit_acc_id,
-        "amount": rand_amount,
-        "currency": "SGD",
-        "memo": "generated from locust",
-        "transaction_date": datetime.now().strftime("%Y-%m-%d"),
-    }
-
-
-def account_query(acc_id: str) -> str:
-    return (
-        " query locust {"
-        f"  getTransactionsForAccount(accountId: '{acc_id}') "
-        "    { id sid amount currency memo status transaction_date }"
-        "}"
-    )
-
-
-def iam_auth_for_service(service: str) -> AWS4Auth:
-    session = boto3.Session()
-    credentials = session.get_credentials()
-    return AWS4Auth(
-        credentials.access_key,
-        credentials.secret_key,
-        session.region_name,
-        service,
-    )
+from api_utils import account_query, iam_auth_for_service, rand_account, rand_fund_transfer_request
 
 
 class ApiUser(HttpUser):
@@ -100,6 +45,9 @@ class ApiUser(HttpUser):
             _, query_account = rand_account()
             print(f"...using random account {query_account} for query")
 
+        # TODO:
+        # appsync returns 200 status when query contains syntax error
+        # need to add logic to handle this
         return self.client.post(
             url=f"{graphql_endpoint}/graphql",
             json={"query": account_query(query_account)},
---
+++ single.py
@@ -0,0 +1,105 @@
+import argparse
+import os
+import pprint
+from typing import Any
+
+import requests
+
+from api_utils import (
+    account_query,
+    fund_transfer_request,
+    iam_auth_for_service,
+    rand_account,
+    rand_fund_transfer_request,
+)
+
+
+def parse_args() -> tuple[dict[str, Any], str]:
+    """parse command line argument and return transfer and query requests to be executed"""
+    parser = argparse.ArgumentParser(description="Send individual requests to server")
+    parser.add_argument("--cust", help="debit account for customer id")
+    parser.add_argument("--debit", help="debit account for transfer request")
+    parser.add_argument("--credit", help="credit account for transfer request")
+    parser.add_argument("--query", help="account for inquiry. any will defaults to --debit if specified")
+    parser.add_argument("--amount", default="100.12", help="amount for transfer")
+    args = parser.parse_args()
+
+    transfer, query, debit_acc_id = {}, "", ""
+
+    if args.debit:
+        if args.debit and args.debit == "any":
+            transfer = rand_fund_transfer_request()
+            debit_acc_id = transfer["debit_account_id"]
+        elif args.debit:
+            transfer = fund_transfer_request(args.cust, args.debit, args.credit, args.amount, "single test")
+            debit_acc_id = transfer["debit_account_id"]
+
+    # if query is set to any, use debit_acc_id if defined by transfer (a natural thing to do)
+    # else random select an account id for query
+    if args.query:
+        if args.query != "any":
+            query = account_query(args.query)
+        elif args.query == "any" and debit_acc_id:
+            query = account_query(debit_acc_id)
+        else:
+            _, acc_id = rand_account()
+            query = account_query(acc_id)
+
+    return transfer, query
+
+
+def do_transfer(transfer: dict[str, Any]) -> bool:
+    print("transfer=>")
+    pprint.pprint(transfer)
+    print()
+
+    rest_endpoint = os.environ.get(
+        "ACCOUNT_TRANSFER_URL", "http://a62f4b0cb4c6e48b4afe48863ed2f921-519988285.us-west-2.elb.amazonaws.com:8080"
+    )
+
+    response = requests.post(
+        url=f"{rest_endpoint}/transfers",
+        headers={"content-type": "application/json"},
+        json=rand_fund_transfer_request(),
+    )
+
+    if response.status_code != 202:
+        print(response)
+        return False
+
+    return True
+
+
+def do_query(query: str) -> bool:
+    print("query=>")
+    print(query)
+
+    graphql_endpoint = os.environ.get(
+        "ACCOUNT_INQUERY_URL", "https://pfl7nvl7xvfulnc2je6hjr35sm.appsync-api.us-west-2.amazonaws.com"
+    )
+
+    response = requests.post(
+        url=f"{graphql_endpoint}/graphql",
+        json={"query": query},
+        auth=iam_auth_for_service("appsync"),
+    )
+
+    if response.status_code != 200:
+        print(response)
+        return False
+
+    body = response.json()
+    if body["data"] is None:
+        print(body)
+        return False
+
+    pprint.pprint(body["data"], width=120, sort_dicts=False, compact=True)
+    return True
+
+
+if __name__ == "__main__":
+    transfer, query = parse_args()
+    if transfer:
+        do_transfer(transfer)
+    if query:
+        do_query(query)
